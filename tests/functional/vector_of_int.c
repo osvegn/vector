4c966ad515c9b7ed7132a08c7bf1b598fbe27b2d
osvegn <thomas.prudhomme@epitech.eu>
2023-08-21 00:13:36 +0200

    [NEW] functional tests

---
+++ tests/functional/vector_of_int.c
@@ -0,0 +1,35 @@
+/*
+ * Filename: vector_of_int.c
+ * Path: tests/fonctional
+ * Created Date: Sunday, August 20th 2023, 12:30:40 pm
+ * Author: osvegn
+ * 
+ * Copyright (c) 2023 Vector
+ */
+
+#include "vector.h"
+#include <stdio.h>
+
+static int my_return(int value, char *message)
+{
+    printf("%s\n", message);
+    fflush(stdout);
+}
+
+int main(void)
+{
+    vector_t vector = {0};
+    int rvalue = vector_constructor(&vector, sizeof(int), 0);
+
+    if (rvalue < 0)
+        return my_return(rvalue, "Constructor failure.");
+    for (int i = 0; i < 10; i++) {
+        rvalue = vector.emplace_back(&vector, &i);
+        if (rvalue < 0)
+            return my_return(rvalue, "Emplace_back failure.");
+    }
+    if (*(int *)vector.front(&vector) != 0)
+        return my_return(rvalue, "Front failure.");
+    vector.destructor(&vector);
+    return 0;
+}
---
+++ tests/functional/vector_of_string.c
@@ -0,0 +1,37 @@
+/*
+ * Filename: vector_of_string.c
+ * Path: tests/fonctional
+ * Created Date: Sunday, August 20th 2023, 11:09:00 pm
+ * Author: osvegn
+ * 
+ * Copyright (c) 2023 Vector
+ */
+
+#include "vector.h"
+#include <string.h>
+#include <stdio.h>
+
+static int my_return(int value, char *message)
+{
+    printf("%s\n", message);
+    fflush(stdout);
+}
+
+int main(void)
+{
+    vector_t vector = {0};
+    int rvalue = vector_constructor(&vector, sizeof(void *), 0);
+    char *av[] = {"./program", "test", "test2"};
+
+    if (rvalue < 0)
+        return my_return(rvalue, "Constructor failure.");
+    for (int i = 0; i < 3; i++) {
+        rvalue = vector.emplace_back(&vector, &av[i]);
+        if (rvalue < 0)
+            return my_return(rvalue, "Emplace_back failure.");
+    }
+    if (strcmp((char *)*(char **)vector.front(&vector), av[0]))
+        return my_return(rvalue, "Front failure.");
+    vector.destructor(&vector);
+    return 0;
+}
