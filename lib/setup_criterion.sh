abbc36c401291720159a732b5f429f4c0f8f22f2
osvegn <thomas.prudhomme@epitech.eu>
2024-10-03 14:18:13 +0200

    Fix workflow documentation

--- lib/setup_criterion.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-url="https://github.com/Snaipe/Criterion/releases/download/v2.4.2/criterion-2.4.2-linux-x86_64.tar.xz"
-name="criterion.tar.xz"
-
-curl -L $url -o $name
-tar -xf $name
-rm $name
